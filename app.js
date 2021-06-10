cd6de5c4e2f3e5e1f5d2df906a51d2c2e4cfb92c
Seizerg <63910941+Seizerg@users.noreply.github.com>
2021-06-10 12:24:37 +0530

    final editing

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.use(bodyparser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 let items =[];
+let work=[];
+let tday;
+let button= "";
+//get req for root route
 app.get("/", function(req,res){
   let day= new Date();
   let options={
@@ -12,15 +16,30 @@ app.get("/", function(req,res){
     day: "numeric",
     month: "long"
   };
-  let tday= day.toLocaleDateString("en-US",options);
-  res.render("list",{day: tday, item: items })
-})
-app.post("/",function(req,res){
-  let newitem= req.body.newItem;
-  items.push(newitem);
-  res.redirect("/");
+   tday= day.toLocaleDateString("en-US",options);
+     res.render("list",{title: "main", item: items })
 
 })
+//get req for work route
+app.get("/work", function(req,res){
+  res.render("list",{title: "work",  item:work});
+})
+//this one for root items
+app.post("/",function(req,res){
+  let newitem= req.body.newItem;
+   button=req.body.btn;
+   if(button==="work"){
+     work.push(newitem);
+     res.redirect("/work");
+
+   }
+   else {
+     items.push(newitem);
+     res.redirect("/");
+   }
+})
+//this one for work items
+
 
 app.listen(3000,function(){
   console.log("server is running");
