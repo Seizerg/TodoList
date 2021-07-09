0eed0ac26c0163adcc8d114c3a616d95f4cec9fb
Seizerg <63910941+Seizerg@users.noreply.github.com>
2021-07-09 20:00:44 +0530

    todolist v2

--- app.js
+++ app.js
@@ -1,122 +1,102 @@
 const express= require("express");
-const bodyparser= require("body-parser");
-const mongoose= require("mongoose");
+const body= require("body-parser");
+const mongoose = require("mongoose");
+const _=require("lodash");
 const app= express();
-app.use(bodyparser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-mongoose.connect("mongodb://localhost:27017/todoDB",{useNewUrlParser:true,useUnifiedTopology:true})
+app.set("view engine","ejs");
+app.use(express.static("public"))
+app.use(body.urlencoded({extended:true}));
+mongoose.connect("mongodb://localhost:27017/todoDB",{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:false})
 const todoSchema= new mongoose.Schema({
-  item:{
-    type:String,
-    required: true
-  }
+  item:String
 });
-const listSchema= new mongoose.Schema({
-  name: String,
+const routeSchema= new mongoose.Schema({
+  name:String,
   content:[todoSchema]
 })
-const List= mongoose.model("Data",listSchema);
-//let work=[];
-//let tday;
-let items=[];
+const Routelist= mongoose.model("Route",routeSchema);
 const Todo= mongoose.model("List",todoSchema);
- const first = new Todo({
-  item:"Brush my Teeth"
+const item1= new Todo({
+  item:"welcome to this TodoList"
 })
-const second= new Todo({
-  item:"Have a coffee"
+const item2= new Todo({
+  item:"You can enter ur items below"
 })
-const third = new Todo({
-  item:"start your coding"
+const item3= new Todo({
+  item:"delete items by pressing -->"
 })
-items=[first, second, third];
-/* Todo.deleteMany({item:["Brush my Teeth","start your coding"]}, function(err, result){
-  (err)?console.log(err):console.log(result);
-}) */
+const defaultItems= [item1,item2,item3];
 
-let button= "";
-//get req for root route
-app.get("/", function(req,res){
-  /* Todo.find({},function(err,result){
+app.get("/",function(req,res){
+  Todo.find({},function(err,result){
     if (result.length===0) {
-      Todo.insertMany([first, second, third], function(err){
+      Todo.insertMany([item1, item2, item3], function(err){
       (err)? console.log(err):console.log("insertion successful");
       res.redirect("/");
     })
   }else {
     res.render("list",{title: "Main", items: result })
   }
-})*/
-Todo.find({},function(err,result){
-  res.render("list",{title: "Main", items: result })
 })
 
-
 })
-app.get("/:routeName",function(req,res){
-  let route= req.params.routeName;
+app.get("/:route",function(req,res){
+  const root= _.capitalize(req.params.route);
 
-  List.findOne({name:route},function(err,root){
-    if(!err){
-    if(!root){
-    let list1= new List({
-        name: route,
-        content:items
-      })
-      list1.save()
-      res.redirect("/"+route);
+ Routelist.findOne({name:root},function(err,rootlist){
+   if(rootlist){
 
-    }else{
-    res.render("list",{title: root.name, items: root.content })
-  }
-}
-  })
-})
-//get req for work route
-/* app.get("/work", function(req,res){
-  res.render("list",{title: "Work",  item:work});
-}) */
-//this one for root items
-app.post("/",function(req,res){
-  let newitem= req.body.newItem;
-  button=req.body.btn;
-  let itm= new Todo({
-    item: newitem
-  })
-  if(button==="Main"){
-  itm.save();
-  res.redirect("/");
-}else {
-  List.findOne({name:button},function(err,foundList){
-    if(!err){
-    foundList.content.push(itm);
-    foundList.save();
-    res.redirect("/"+button);
-  }
-  })
-}
-   })
-    app.post("/delete",function(req,res){
-      console.log(req.body);
-     const checkedItem=req.body.button;
-     Todo.findByIdAndRemove(checkedItem,function(err){
-       (err)?console.log(err):res.redirect("/");
+     res.render("list",{title: rootlist.name, items: rootlist.content })
+   }else{
+     const rootRoute= new Routelist({
+       name:root,
+       content:defaultItems
      })
-   })
-   /* if(button==="work"){
-     work.push(newitem);
-     res.redirect("/work");
-
+     rootRoute.save();
+     res.redirect("/"+root);
    }
-   else {
-     items.push(newitem);
-     res.redirect("/");
-   } */
+ })
 
-//this one for work items
+})
+//when user input new items in the list
+app.post("/",function(req,res){
+  const input= req.body.newItem;
+  const btnvalue= req.body.btn;
+  const newitm= new Todo({
+    item:input
+  })
+  if(btnvalue==="Main"){
+    newitm.save();
+    res.redirect("/");
+  }else{
+  Routelist.findOne({name:btnvalue},function(err,result){
+  if(result){
+  result.content.push(newitm)
+  result.save();
+  res.redirect("/"+btnvalue);
+  }
+})
+}
+})
 
 
-app.listen(3000,function(){
-  console.log("server is running");
+//when user tried to delete item from his list
+app.post("/delete",function(req,res){
+  const id= req.body.button;
+  const rootinfo=req.body.rootinfo;
+  if(rootinfo==="Main"){
+    Todo.findByIdAndRemove({_id:id},function(err){
+      (err)?console.log(err):res.redirect("/");
+    })
+  }else{
+  Routelist.findOneAndUpdate({name:rootinfo},{$pull:{content:{_id:id}}},function(err,result){
+     result.save();
+     res.redirect("/"+rootinfo);
+
+   })
+  }
+  })
+
+app.listen(3000, function(){
+  console.log("server started");
 })
